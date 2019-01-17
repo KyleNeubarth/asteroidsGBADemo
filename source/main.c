51a8d1fffdbfacec9833c5b7fa70508ada5928f1
KyleNeubarth <kyle@neubarth.org>
2019-01-17 12:55:38 -0700

    changed ship driving, added bullets, added wrapping for bullets

--- source/main.c
+++ source/main.c
@@ -9,7 +9,7 @@ OBJ_ATTR obj_buffer[128];
 OBJ_AFFINE *obj_aff_buffer= (OBJ_AFFINE*)obj_buffer;
 
 int numBullets = 0;
-float bulletCoords[10][2];
+float bulletCoords[10][3];
 
 void spawnBullet(float x, float y) {
 	numBullets++;
@@ -26,17 +26,16 @@ int main() {
 
 	obj_set_attr(obj_buffer,ATTR0_SQUARE | ATTR0_AFF_DBL,ATTR1_SIZE_32x32 | ATTR1_AFF_ID(0),ATTR2_PALBANK(0) );
 
-	OBJ_ATTR* bullet = &obj_buffer[1];
 	//aff id is still 0 because it will share the orientation of the ship
 	REG_DISPCNT= DCNT_MODE1 | DCNT_OBJ | DCNT_OBJ_1D;
 
-	obj_set_pos(obj_buffer,SCREEN_WIDTH/2-16,SCREEN_WIDTH/2-16);
+	obj_set_pos(obj_buffer,SCREEN_WIDTH/2-16,SCREEN_HEIGHT/2-16);
 
 	float x = SCREEN_WIDTH/2-32;
 	float y = SCREEN_HEIGHT/2-32;
 
 	u16 rot = 0;
-	int scale = 320;
+	int scale = 640;
 
 	float speed = 0;
 	float friction = .99f;
@@ -48,7 +47,7 @@ int main() {
 		rot += key_tri_horz();
 		scale += key_tri_vert();
 		if (key_held(1<<KI_A)) {
-			speed += .04f;
+			speed += .02f;
 		}
 		speed *= friction;
 		//holy shit this is inefficient
@@ -58,21 +57,51 @@ int main() {
 		y += speed*cos/(float)6000;
 
 		if (key_hit(1<<KI_B) && numBullets < 10) {
-			float bx = x + 32 + 50*sin/(float)6000;
-			float by = y + 32 + 50*cos/(float)6000;
+			float bx = x + 32 - 4 + 5*sin/(float)6000;
+			float by = y + 32 - 4 + 5*cos/(float)6000;
 			bulletCoords[numBullets][0] = bx;
 			bulletCoords[numBullets][1] = by;
+			bulletCoords[numBullets][2] = rot;
 			spawnBullet(bx,by);
 			obj_aff_rotscale(&obj_aff_buffer[numBullets],scale,scale,rot*500);
 		}
 		for (int i=1;i<=numBullets;i++) {
 			float bx = bulletCoords[i-1][0];
 			float by = bulletCoords[i-1][1];
-			obj_set_pos(&obj_buffer[i],bx+1,by);
-			bulletCoords[i-1][0]++;
+			int brot = bulletCoords[i-1][2];
+			bx += 3*lu_sin(500*brot)/(float)6000;
+			by += 3*lu_cos(500*brot)/(float)6000;
+			obj_set_pos(&obj_buffer[i],bx,by);
+			bulletCoords[i-1][0] = bx;
+			bulletCoords[i-1][1] = by;
+		}
+		for (int i=1;i<11;i++) {
+			int nx = obj_buffer[i].attr1 & 0xff;
+			if (nx > 240) {
+				nx = 0;
+				bulletCoords[i-1][0] = 0;
+				obj_buffer[i].attr1 &= ~0xff;
+				obj_buffer[i].attr1 |= nx;
+			} else if (nx<1) {
+				nx = 240;
+				bulletCoords[i-1][0] = 240;
+				obj_buffer[i].attr1 &= ~0xff;
+				obj_buffer[i].attr1 |= nx;
+			}
+			int ny = obj_buffer[i].attr0 & 0xff;
+			if (ny > 160) {
+				ny = 0;
+				bulletCoords[i-1][1] = 0;
+				obj_buffer[i].attr0 &= ~0xff;
+				obj_buffer[i].attr0 |= ny;
+			} else if (ny<1) {
+				ny = 160;
+				bulletCoords[i-1][1] = 160;
+				obj_buffer[i].attr0 &= ~0xff;
+				obj_buffer[i].attr0 |= ny; 
+			}
 
 		}
-
 		obj_aff_rotscale(obj_aff_buffer,scale,scale,rot*500);
 		obj_set_pos(obj_buffer,x,y);
 		oam_copy(oam_mem, obj_buffer, 128);
